882066a3c1aa3db23e281e3ff2ed0b19e8e4d73a
Rekesh Ali <rekeshali@gmail.com>
2021-10-13 16:30:39 -0500

    added phrases to captainrig

--- captainrig.py
+++ captainrig.py
@@ -4,6 +4,11 @@ import logging
 from phrases import *
 from rigreboot import rebootHighwind, rebootTrailblazer, bootHighwind, bootTrailblazer
 
+def is(keyword,name,text):
+    return name in text.lower() and keyword in text.lower()
+
+
+
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 high, trail = False, False
 class MyClient(discord.Client):
@@ -18,60 +23,70 @@ class MyClient(discord.Client):
             return
         text = message.content
         logging.info('%s: %s'%(message.author.name,text))
+        # Automatic actions
         if message.author.name == 'Hive Bot':
-            if 'highwind' in text.lower() and 'offline' in text.lower():
+           
+            if is('offline','highwind',text):
                 if self.high:
                     self.high = False
                     return
-                await message.channel.send('Blimey! Highwind full sail lads!')
+                await message.channel.send(auto_highwind)
                 logging.info('Rebooting highwind.')
                 rebootHighwind(10)
-            elif 'trailblazer' in text.lower() and 'offline' in text.lower():
+
+            if is('offline','trailblazer',text):
                 if self.trail:
                     self.trail = False
                     return
-                await message.channel.send('Scurvy dog! Trailblazer full speed ahead matey!')
+                await message.channel.send(auto_trailblazer)
                 logging.info('Rebooting Trailblazer.')
                 rebootTrailblazer(10)
-            elif 'chariot' in text.lower() and 'offline' in text.lower():
-                await message.channel.send('Despicable! Chariot be damned!')
-            elif 'orphan' in text.lower() and 'offline' in text.lower():
-                await message.channel.send('Thar she blows! And a right foul orphan ye arrr!')
+
+            if is('offline','chariot',text):
+                await message.channel.send(auto_chariot)
+            if is('offline','orphan',text):
+                await message.channel.send(auto_orphan)
+
+        # User actions
         else:
-            if 'highwind' in text.lower() and 'reboot' in text.lower():
+
+            if is('reboot','highwind',text):
                 if message.author.name == 'redeye':
-                    await message.channel.send('Why yes master! As you command! Highwind full sail, put yer back into it lads!')
+                    await message.channel.send(master_highwind)
                 else:
-                    await message.channel.send("Watch yer tongue ye wench! I'll have it fer stew! Ye herd'im lads...")
+                    await message.channel.send(grunt_highwind)
                 logging.info('Rebooting Highwind.')
                 self.high = True
                 rebootHighwind(10)
             elif 'highwind' in text.lower() and 'boot' in text.lower():
                 if message.author.name == 'redeye':
-                    await message.channel.send('Why yes master! As you command! Highwind full sail, put yer back into it lads!')
+                    await message.channel.send(master_highwind)
                 else:
-                    await message.channel.send("Watch yer tongue ye wench! I'll have it fer stew! Ye herd'im lads...")
+                    await message.channel.send(grunt_highwind)
                 logging.info('Booting Highwind.')
                 bootHighwind()
+
             if 'trailblazer' in text.lower() and 'reboot' in text.lower():
                 if message.author.name == 'redeye':
-                    await message.channel.send('Oh senpai! I am not worthy of your gaze! Trailblazer full speed ahead, drop the sails ye scalliwags!')
+                    await message.channel.send(master_trailblazer)
                 else:
-                    await message.channel.send("May Davy Jones claim ye wretched soul! Make haste lads...")
+                    await message.channel.send(grunt_trailblazer)
                 logging.info('Rebooting Trailblazer.')
                 self.trail = True
                 rebootTrailblazer(10)
+
             elif 'trailblazer' in text.lower() and 'boot' in text.lower():
                 if message.author.name == 'redeye':
-                    await message.channel.send('Oh senpai! I am not worthy of your gaze! Trailblazer full speed ahead, drop the sails ye scalliwags!')
+                    await message.channel.send(master_trailblazer)
                 else:
-                    await message.channel.send("May Davy Jones claim ye wretched soul! Make haste lads...")
+                    await message.channel.send(grunt_trailblazer)
                 logging.info('Booting Trailblazer.')
                 bootTrailblazer()
+            
             if 'chariot' in text.lower() and 'reboot' in text.lower():
-                await message.channel.send("HARHARHAR, yo ho ho! Quite the tale ye spin.")
+                await message.channel.send(any_chariot)
             if 'orphan' in text.lower() and 'reboot' in text.lower():
-                await message.channel.send("Ye landlubbers will hange the jib or I'll hang'em from the Yardam!")
+                await message.channel.send(any_chariot)
 
 with open('token') as f:
     token = f.readline()
--- phrases.py
+++ phrases.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-autoHighwind      = 'Blimey! Highwind full sail lads!'
-autoTrailblazer   = 'Scurvy dog! Trailblazer full speed ahead matey!'
-autoChariot       = 'Despicable! Chariot be damned!'
-autoOrphan        = 'Thar she blows! And a right foul orphan ye arrr!'
-masterHighwind    = 'Why yes master! As you command! Highwind full sail, put yer back into it lads!'
-gruntHighwind     = "Watch yer tongue ye wench! I'll have it fer stew! Ye herd'im lads..."
-masterTrailblazer = 'Oh senpai! I am not worthy of your gaze! Trailblazer full speed ahead, drop the sails ye scalliwags!'
-gruntTrailblazer  = "May Davy Jones claim ye wretched soul! Make haste lads..."
-anyChariot        = "HARHARHAR, yo ho ho! Quite the tale ye spin."
-anyOrphan         = "Ye landlubbers will hange the jib or I'll hang'em from the Yardam!"
+auto_highwind      = 'Blimey! Highwind full sail lads!'
+auto_trailblazer   = 'Scurvy dog! Trailblazer full speed ahead matey!'
+auto_chariot       = 'Despicable! Chariot be damned!'
+auto_orphan        = 'Thar she blows! And a right foul orphan ye arrr!'
+master_highwind    = 'Why yes master! As you command! Highwind full sail, put yer back into it lads!'
+grunt_highwind     = "Watch yer tongue ye wench! I'll have it fer stew! Ye herd'im lads..."
+master_trailblazer = 'Oh senpai! I am not worthy of your gaze! Trailblazer full speed ahead, drop the sails ye scalliwags!'
+grunt_trailblazer  = "May Davy Jones claim ye wretched soul! Make haste lads..."
+any_chariot        = "HARHARHAR, yo ho ho! Quite the tale ye spin."
+any_orphan         = "Ye landlubbers will hange the jib or I'll hang'em from the Yardam!"
